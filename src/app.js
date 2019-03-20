67d9a8a6c3527562739c6168e1ddcb172f6fcadb
cgs3300 <cgs3300@163.com>
2019-03-20 14:43:09 +0800

    20190320

---
+++ src/app.js
@@ -0,0 +1,14 @@
+const http = require('http');
+const chalk = require('chalk');
+const conf = require('./config/defaultConfig');
+
+const server = http.createServer((req,res) =>{
+    res.statusCode = 200;
+    res.end('hello world.');
+});
+
+server.listen(conf.hostname,() => {
+    const addr = `http://${conf.hostname}:${conf.port}`;
+    console.info(`Server started at ${chalk.green(addr)}`);
+    
+});
